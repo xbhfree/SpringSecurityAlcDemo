2c4249b54d4547c7c3dbbee9895434ab3e957876
xbhfree <xbhfree@163.com>
2023-12-27 04:51:22 +0800

    Initial commit

---
+++ src/main/java/com/example/springsecurityalcdemo/SpringSecurityAlcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityalcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAlcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAlcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsecurityalcdemo/SpringSecurityAlcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityalcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAlcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
