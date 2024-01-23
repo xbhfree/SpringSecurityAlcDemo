3d81c6061187286bde32f57dbeca19ae094a690f
xbhfree <xbhfree@163.com>
2024-01-24 06:12:17 +0800

    微服务权限案例-编写security授权过滤器

--- common/spring_security/src/main/java/com/example/entity/SecurityUser.java
+++ common/spring_security/src/main/java/com/example/entity/SecurityUser.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 @Data
---
+++ common/spring_security/src/main/java/com/example/filter/TokenAuthFilter.java
@@ -0,0 +1,67 @@
+package java.com.example.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.com.example.security.TokenManager;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class TokenAuthFilter extends BasicAuthenticationFilter {
+    private TokenManager tokenManager;
+    private RedisTemplate redisTemplate;
+    private AuthenticationManager authenticationManager;
+    private SecurityContextHolderStrategy securityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy();
+
+    public TokenAuthFilter(AuthenticationManager authenticationManager, TokenManager tokenManager, RedisTemplate redisTemplate) {
+        super(authenticationManager);
+        this.tokenManager = tokenManager;
+        this.redisTemplate = redisTemplate;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        //获取当前认证成功用户权限信息
+        Authentication authRequest = getAuthentication(request);
+        //如果有结果，放到权限上下文中
+        if (authRequest != null){
+            Authentication authResult = this.authenticationManager.authenticate(authRequest);
+            SecurityContext context = this.securityContextHolderStrategy.createEmptyContext();
+            context.setAuthentication(authResult);
+            this.securityContextHolderStrategy.setContext(context);
+        }
+    }
+
+    private Authentication getAuthentication(HttpServletRequest request) {
+        //从head获取token
+        String token = request.getHeader("token");
+        if (token != null){
+            //从token获取用户名
+            String username = tokenManager.getUserInfoFromToken(token);
+            //从redis获取权限表
+            List<String> permissionList = (List<String>)redisTemplate.opsForValue().get(username);
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            for (String perm : permissionList) {
+                SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(perm);
+                authorities.add(simpleGrantedAuthority);
+            }
+            return new UsernamePasswordAuthenticationToken(username,token,authorities);
+        }
+        return null;
+    }
+}
